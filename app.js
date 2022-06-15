3ee1fcedb00d3b5ff641916e6604b0ab3c24d84c
Jose David <josedbolivarmafrontend@gmail.com>
2022-06-15 10:58:53 -0500

    Inquirer.js

---
+++ app.js
@@ -0,0 +1,38 @@
+require('colors');
+const { 
+    inquirerMenu,
+     pausa,
+     leerInput
+    } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+const main = async () => {
+    let opt = '';
+    const tareas  = new Tareas();
+
+    do {
+      opt = await inquirerMenu();
+      console.log({ opt });
+      console.log('MENUS')
+      console.log('app.js - LISTADO TAREAS: ',tareas._listado);
+
+      switch (opt) {
+        case '1':
+            // Crear opción
+            const desc = await leerInput('Descripción: ');
+            tareas.crearTarea( desc );
+          break;
+
+        case '2':
+          console.log( tareas._listado );
+        break;
+      }
+      
+      await pausa();
+      
+    } while (opt !== '0');
+    
+
+  }
+    
+main();
